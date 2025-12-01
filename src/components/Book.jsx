e6db123b8c4f77dfa374a461ff149f30642e7e8d
George Zisis <georgezisis@gmail.com>
2025-12-01 13:34:48 +0200

    Add info to shop card

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./Book.css";
 
 export default function Book({ image, text }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="book">
-      <img src={image} alt="book1" />
-      <div className="text">{text}</div>
+    <div className={`book ${isOpen ? "show" : ""}`}>
+      <div className="visible-info">
+        <img src={image} alt="book1" />
+        <button className="toggle-button" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? "Απόκρυψη" : "Πληροφορίες"}
+        </button>
+      </div>
+      <div className={`text ${isOpen ? "show" : ""}`}>{text}</div>
     </div>
   );
 }
