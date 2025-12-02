a61fc35b8d25b940ce619c6582718ddc7f8ba8c6
George Zisis <georgezisis@gmail.com>
2025-12-02 10:52:31 +0200

    Create books file, make only one card appear at a time

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
 import "./Book.css";
 
-export default function Book({ image, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function Book({ book, isOpen, onToggle }) {
   return (
     <div className={`book ${isOpen ? "show" : ""}`}>
       <div className="visible-info">
-        <img src={image} alt="book1" />
-        <button className="toggle-button" onClick={() => setIsOpen(!isOpen)}>
+        <img src={book.image} alt="book1" />
+        <button className="toggle-button" onClick={onToggle}>
           {isOpen ? "Απόκρυψη" : "Πληροφορίες"}
         </button>
       </div>
-      <div className={`text ${isOpen ? "show" : ""}`}>{text}</div>
+      <div className={`text ${isOpen ? "show" : ""}`}>{book.content}</div>
     </div>
   );
 }
